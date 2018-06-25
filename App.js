689b3cc9f5112f7d50748f6d003e88823334ee48
unknown <braianchincho@gmail.com>
2018-06-25 20:23:22 -0300

    primer commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+import PhotoList from './src/components/PhotoList';
+import { Router, Scene } from 'react-native-router-flux';
+
+const App = () => (
+  <Router>
+    <Scene key="root">
+      <Scene key="albumList" component={AlbumList} title="Albums" initial={true} />
+      <Scene key="photoList" component={PhotoList} title="Photos" />
+    </Scene>
+  </Router>
+);
+
+export default App;
+
+AppRegistry.registerComponent('albums', () => App);
+
+  
